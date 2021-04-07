de868b0d2aa25c9338a3a0ab309efc46ce0c9166
Matheus Souza Santana <matheus.santana@splendasa.com.br>
2021-04-07 14:37:06 -0300

    Com parametro

--- src/components/basicos/Primeiro.js
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-export default function Primeiro() {
-    const msg = 'Primeiro Componete'
-}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import React from 'react'
 import './index.css';
 
 import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/Primeiro';
 
 ReactDOM.render(
-    <div>
+    <div id="app">
         <Primeiro> </Primeiro>
-        <Primeiro> </Primeiro>
+        <ComParametro
+        titulo="Situação do aluno"
+        aluno="Pedro Silva"
+        nota={9.2} />
     </div>,
     document.getElementById('root') 
 )   
